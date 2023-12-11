637aa2603b2b7b11dd217b83b18c09a63a49c1f2
Antonio Marín <antoniomarmart3@gmail.com>
2023-12-11 16:59:17 +0100

    Clase monstruo con sus atributos y constructor

---
+++ DungeonQuest/src/Monstruo.java
@@ -0,0 +1,12 @@
+package DungeonQuest.src;
+
+public class Monstruo {
+    protected String nombre;
+    protected int nivel;
+
+    public Monstruo(String nombre, int nivel) {
+        this.nombre = nombre;
+        this.nivel = nivel;
+    }
+
+}
