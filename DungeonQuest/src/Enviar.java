2cf1e8e6887b04a5aebf03e6cb03d28ecc6c4061
Luis Paños Cuenca <115982393+LuisPacu10@users.noreply.github.com>
2023-12-18 13:06:32 +0100

    Cambios en mensaje aventurero

--- DungeonQuest/src/Enviar.java
+++ DungeonQuest/src/Enviar.java
@@ -134,11 +134,12 @@ public class Enviar extends Thread {
 
         try {
             //Creamos el socket de UDP
-            String filePath ="C:/Users/pablo/IdeaProjects/Proyecto-JuegoRol/" +"xml_"+mensajeAEnviar.msgId+".xml";
+            String filePath ="C:/Users/luisp/IdeaProjects/Proyecto-JuegoRol/" +"xml_"+mensajeAEnviar.msgId+".xml";
             /*
             Ruta para:
             Pablo = C:\Users\pablo\IdeaProjects\Proyecto-JuegoRol\out\production\Proyecto-JuegoRol\DungeonQuest
             Antonio = C:/Users/marti/IdeaProjects/SMA_23-24/Proyecto/
+            Luis = C:\Users\luisp\IdeaProjects\Proyecto-JuegoRol
              */
             String fileName = new File(filePath).getName();
             DatagramSocket socketUDP = new DatagramSocket();
